a5ba5a1793945c801cd83c713e9843d6c0242187
EmilyATheodorakos <96351819+EmilyATheodorakos@users.noreply.github.com>
2021-12-18 12:50:39 -0700

    Add files via upload

---
+++ Linux/Player_Dealer_Correlation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cat $1_Dealer_schedule | awk -F" " '{print $1,$2,$5,$6}' | grep $2    # Your init script
+#
+# Atom will evaluate this file each time a new window is opened. It is run
+# after packages are loaded/activated and after the previous editor state
+# has been restored.
+#
+# An example hack to log to the console when each text editor is saved.
+#
+# atom.workspace.observeTextEditors (editor) ->
+#   editor.onDidSave ->
+#     console.log "Saved! #{editor.getPath()}"
